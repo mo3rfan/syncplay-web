1b81da28b108368d3e8b19684bc447969a4da453
Irfan <PowerInside@users.noreply.github.com>
2017-04-22 21:08:31 +0530

    Add websock client library
    This is necessary to interact with the newer
    websockify websockets bridge. The deprecation
    of binary base64 encoding in websockify 0.5.0+
    required the use of this shim.

---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+const webpack = require('webpack'); //to access built-in plugins
+const path = require('path');
+
+const config = {
+  entry: './src/syncplay.js',
+  output: {
+    path: path.resolve(__dirname, 'build'),
+    filename: 'syncplay.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/, 
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            plugins: ["transform-flow-strip-types", "transform-remove-console"],
+            presets: ['env']
+          }
+        }
+      }
+    ]
+  }
+}
+
+module.exports = config;
